c3effeb35ef7181a6402ef9b06687577216ce57e
SATHEESH <satheesh@dreamhost.in>
2017-11-04 19:26:41 +0530

    update passport

--- _apps/routes/api.php
+++ _apps/routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+
+Route::group(['namespace' => 'api'], function () {
+    Route::get('/login', 'UserController@login');
 });
